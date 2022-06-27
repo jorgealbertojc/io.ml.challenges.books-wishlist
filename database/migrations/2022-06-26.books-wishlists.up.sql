209e013f574ccfc04e357b8943dbd10297f6b85b
arkosnoemarenom <219255+jorgealbertojc@users.noreply.github.com>
2022-06-26 19:14:41 -0500

    feat(#4, #5, #6): added tables books wishlist design
    - users signin tokens
    - books wishlists
    - books wishlist items

---
+++ database/migrations/2022-06-26.books-wishlists.up.sql
@@ -0,0 +1,9 @@
+
+
+
+CREATE TABLE IF NOT EXISTS books_wishlists (
+    _id VARCHAR(36),
+    spec_user_id VARCHAR(36),
+    spec_name VARCHAR(100),
+    spec_description VARCHAR(300)
+);
---
+++ database/migrations/2022-06-26.users.signin.tokens.down.sql
@@ -0,0 +1,4 @@
+
+
+
+DROP TABLE IF EXISTS users_signing_tokens ;
